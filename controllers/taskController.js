257a88c8aa5fcbfc1bcf91131e5826723d09a99b
krushna <you@example.com>
2025-07-11 01:15:50 +0530

    Initial commit

---
+++ controllers/taskController.js
@@ -0,0 +1,22 @@
+const Task = require('../models/Task');
+
+exports.getTasks = async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+};
+
+exports.createTask = async (req, res) => {
+  const newTask = new Task(req.body);
+  const savedTask = await newTask.save();
+  res.status(201).json(savedTask);
+};
+
+exports.updateTask = async (req, res) => {
+  const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updatedTask);
+};
+
+exports.deleteTask = async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Task deleted' });
+};
